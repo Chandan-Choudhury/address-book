0b85bb6221c6e453eda89f063b2d7a237fb2ee66
Chandan Choudhury <chandanchoudhury0809@gmail.com>
2022-11-20 10:47:24 +0530

    Added new routes for get address by city name

--- main.py
+++ main.py
@@ -82,44 +82,64 @@ def get_address_by_id(id:int, session: Session = Depends(get_session)):
         return JSONResponse(status_code=500, content={"message": "Something went wrong."})
 
 
-@app.get("/{longitude}/{latitude}")
-# This is a path operation function to get a specific address from DB by longitude and latitude
-def get_addressess_by_location(longitude:float, latitude:float, session: Session = Depends(get_session)):
+@app.get("/location/{city}")
+# This is a path operation function to get a specific address from DB by city name
+def get_address_by_city(city:str, session: Session = Depends(get_session)):
     try:
-        # Query the address from DB by longitude and latitude
-        addresses_by_location = session.query(models.Address).filter(models.Address.longitude == longitude, models.Address.latitude == latitude).all()
-        # Log the addresses to the console
-        print("Addresses by location : ",addresses_by_location)
-        # Check if there are any addresses with the given longitude and latitude in the DB
-        if addresses_by_location == []:
-            # If there are no addresses with the given longitude and latitude in the DB, return a JSON response with status code 404 and a message
-            return JSONResponse(status_code=404, content={"message": "No address found for the given location."})
+        # Query the address from DB by city name
+        address_by_city = session.query(models.Address).filter(models.Address.city == city).all()
+        # Log the address to the console
+        print("Address by city : ",address_by_city)
+        # Check if there is an address with the given city name in the DB
+        if address_by_city == []:
+            # If there is no address with the given city name in the DB, return a JSON response with status code 404 and a message
+            return JSONResponse(status_code=404, content={"message": "No address found with the given city name."})
         else:
-            # If there are addresses with the given longitude and latitude in the DB, return a JSON response with status code 200 and the addresses
-            return addresses_by_location
+            # If there is an address with the given city name in the DB, return a JSON response with status code 200 and the address
+            return address_by_city
     except:
         # If there is an error, return a JSON response with status code 500 and a message
         return JSONResponse(status_code=500, content={"message": "Something went wrong."})
 
 
-@app.get("/{longitude}/{latitude}/{radius}")
-# This is a path operation function to get a specific address from DB by longitude and latitude and radius(in KM)
-# Here user need to share his logituude and latitude and a radius value in KM to check if there are any addresses in the given radius from his location
-def get_addressess_by_location_and_radius(longitude:float, latitude:float, radius:float, session: Session = Depends(get_session)):
+@app.get("/coordinates/{longitude}/{latitude}")
+# This is a path operation function to get a specific address from DB by longitude and latitude
+def get_addressess_by_coordinates(longitude:float, latitude:float, session: Session = Depends(get_session)):
     try:
-        # Convert the radius from KM to degrees (Let's take 1 degree = 111 KM as reference here)
-        radius_in_degree = radius / 111
-        # Log the radius in degrees to the console
-        print("Latitude/Logitude radius in degrees :", radius_in_degree)
-        # Query the addresses from DB by longitude and latitude and radius
-        addresses_by_location_and_radius = session.query(models.Address).filter(models.Address.longitude.between(longitude-radius_in_degree, longitude+radius_in_degree), models.Address.latitude.between(latitude-radius_in_degree, latitude+radius_in_degree)).all()
-        # Check if there are any addresses with the given longitude and latitude and radius in the DB
-        if addresses_by_location_and_radius == []:
-            # If there are no addresses with the given longitude and latitude and radius in the DB, return a JSON response with status code 404 and a message
-            return JSONResponse(status_code=404, content={"message": "No address found for the given location and radius."})
+        # Query the address from DB by longitude and latitude
+        addresses_by_coordinates = session.query(models.Address).filter(models.Address.longitude == longitude, models.Address.latitude == latitude).all()
+        # Log the addresses to the console
+        print("Addresses by coordinates : ",addresses_by_coordinates)
+        # Check if there are any addresses with the given longitude and latitude in the DB
+        if addresses_by_coordinates == []:
+            # If there are no addresses with the given longitude and latitude in the DB, return a JSON response with status code 404 and a message
+            return JSONResponse(status_code=404, content={"message": "No address found for the given location."})
         else:
-            # If there are addresses with the given longitude and latitude and radius in the DB, return a JSON response with status code 200 and the addresses
-            return addresses_by_location_and_radius
+            # If there are addresses with the given longitude and latitude in the DB, return a JSON response with status code 200 and the addresses
+            return addresses_by_coordinates
+    except:
+        # If there is an error, return a JSON response with status code 500 and a message
+        return JSONResponse(status_code=500, content={"message": "Something went wrong."})
+
+
+@app.get("/coordinates-distance/{longitude}/{latitude}/{distance}")
+# This is a path operation function to get a specific address from DB by longitude and latitude and distance(in KM)
+# Here user need to share his logituude and latitude and a distance value in KM to check if there are any addresses in the given distance from his location
+def get_addressess_by_coordinates_and_distance(longitude:float, latitude:float, distance:float, session: Session = Depends(get_session)):
+    try:
+        # Convert the distance from KM to degrees (Let's take 1 degree = 111 KM as reference here)
+        distance_in_degree = distance / 111
+        # Log the distance in degrees to the console
+        print("Latitude/Logitude distance in degrees :", distance_in_degree)
+        # Query the addresses from DB by longitude and latitude and distance
+        addresses_by_coordinates_and_distance = session.query(models.Address).filter(models.Address.longitude.between(longitude-distance_in_degree, longitude+distance_in_degree), models.Address.latitude.between(latitude-distance_in_degree, latitude+distance_in_degree)).all()
+        # Check if there are any addresses with the given longitude and latitude and distance in the DB
+        if addresses_by_coordinates_and_distance == []:
+            # If there are no addresses with the given longitude and latitude and distance in the DB, return a JSON response with status code 404 and a message
+            return JSONResponse(status_code=404, content={"message": "No nearby addresses found for the given location and distance."})
+        else:
+            # If there are addresses with the given longitude and latitude and distance in the DB, return a JSON response with status code 200 and the addresses
+            return addresses_by_coordinates_and_distance
     except:
         # If there is an error, return a JSON response with status code 500 and a message
         return JSONResponse(status_code=500, content={"message": "Something went wrong."})
